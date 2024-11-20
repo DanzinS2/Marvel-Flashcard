66a49c2daabf6b5838a30cb16287239e95891f1b
DanzinS2 <141967869+DanzinS2@users.noreply.github.com>
2024-11-20 18:24:48 -0300

    Add files via upload
    xD

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Quadrinhos',
+    'Qual foi o primeiro super-herói criado pela Marvel Comics, e em qual edição ele apareceu pela primeira vez?',
+    ' O primeiro super-herói da Marvel foi o Tocha Humana original (Jim Hammond), que apareceu na edição Marvel Comics #1 em 1939, publicada pela Timely Comics (precursora da Marvel).'
+)
+
+criaCartao(
+    'Filmes (MCU)',
+    'Em Vingadores: Ultimato, qual é a frase icônica dita por Tony Stark antes de estalar os dedos com a Manopla do Infinito?',
+    '"Eu sou o Homem de Ferro."'
+)
+
+criaCartao(
+    'Séries',
+    'Na série Loki, qual é o nome da organização responsável por controlar as linhas do tempo e garantir que não ocorram ramificações indesejadas?',
+    'Autoridade de Variação Temporal (AVT).'
+)
+
+criaCartao(
+    'Personagens',
+    'Qual personagem da Marvel é conhecido como o "Mercenário Tagarela" e frequentemente quebra a quarta parede?',
+    'Deadpool.'
+)
